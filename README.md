81c09d9353d310d52d86f729c8638347a6075d70
Daniel Shugarts <daniel.shugarts@gmail.com>
2018-02-28 19:11:23 -0600

    BP display created & readme updated

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # sp_build_1
+
+- change nicotine in dbms to boolean
+- remove vo2
+- remove c-rp
--- server/public/scripts/controllers/form.controller.js
+++ server/public/scripts/controllers/form.controller.js
@@ -2,10 +2,17 @@ app.controller('FormController', ['HealthService', function(HealthService) {
     console.log('FormController created.');
     let self = this;
 
-    self.newData = {};
-
+  self.newData = {};
+  
     self.createData = function(){
       console.log(self.newData);
+      console.log(self.newData.systolic);
+      self.newObject = {
+        systolic: self.newData.systolic,
+        diastolic: self.newData.diastolic,
+        blood_pressure: self.newData.systolic + "/" + self.newData.diastolic
+      };
+      console.log(self.newObject);
     };
 
 
